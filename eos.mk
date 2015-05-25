12f53ecc1edc76dd51077bea0011a78ac632d9ec
Glen D'souza <glndsouza2691995@gmail.com>
2015-05-25 17:06:02 +0000

    Initial EOS bringup

--- eos.mk
+++ eos.mk
@@ -1,7 +1,11 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common EOS stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+vendor/eos/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := eos_falcon
